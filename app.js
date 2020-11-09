8040afca7ab717f235806944daae91520e2457ef
davidwood3 <davidwood3@gmail.com>
2020-11-09 11:26:27 -0800

    With body parser added

--- app.js
+++ app.js
@@ -2,16 +2,31 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get("/", function (req, res) {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=312f190d88660f2eba48a6db7dd00742";
+  res.sendFile(__dirname + "/index.html");
+});
 
-  https.get(url, function(response){
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const apiKey = "312f190d88660f2eba48a6db7dd00742";
+  const unit = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&APPID=" + apiKey + "&units=" + unit;
+  
+  console.log(url);
+  
+  https.get(url, function (response) {
     console.log(response.statusCode);
-
-    response.on("data", function(data) {
+  
+    response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       console.log(temp);
@@ -20,13 +35,16 @@ app.get("/", function (req, res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write("<p>The weather is currently: " + weatherDescription + "</p>");
-      res.write("<h1>The temperature in London is: " + temp + " degrees Celcius.</h1>");
+      res.write("<h1>The temperature in " + query + " is: " + temp + " degrees Farenheit.</h1>");
       res.write("<img src=" + imageURL + ">");
       res.send();
-    })
-  })
+    });
+  });
   
 });
+
+
+
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
